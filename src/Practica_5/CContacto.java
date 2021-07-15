7ed30e72fd45c5dd4d685237f0e2405418e84e37
mjackson22-bit <jm5444859@gmail.com>
2021-07-14 21:47:45 -0600

    Archivos iniciales

---
+++ src/Practica_5/CContacto.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica_5;
+
+/**
+ *
+ * @author Jackson
+ */
+public class CContacto {
+    private String contacto;
+    private String nombreApellido;
+    private String correo;
+    private String telefono;
+    private String direccion;
+    private String grupo;
+    
+    public CContacto(){
+        
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getNombreApellido() {
+        return nombreApellido;
+    }
+
+    public void setNombreApellido(String nombreApellido) {
+        this.nombreApellido = nombreApellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+}
